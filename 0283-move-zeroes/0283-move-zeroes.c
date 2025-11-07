82da48f1e8f5ac9e10ac2ce28b82e24f3e8af2db
notins <nitin.somisetty@gmail.com>
2025-11-07 22:09:23 +0530

    Time: 0 ms (100%), Space: 19.6 MB (88.46%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.c
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -1,22 +1,11 @@
 void moveZeroes(int* nums, int numsSize) {
-    int newarr[numsSize];
-    int j =0 ; int count=0;
-    for(int i=0;i<numsSize;i++)
-    {
-        if(nums[i]!=0)
-        {
-            newarr[j++]=nums[i];
-            count++;
+    int j = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != 0) {
+            nums[j++] = nums[i];
         }
     }
-    int k=numsSize-count;
-    while(k--)
-    {
-        newarr[j++]=0;
-    }
-    //put it back to nums
-    for(int i=0; i<numsSize;i++)
-    {
-        nums[i]=newarr[i];
+    while (j < numsSize) {
+        nums[j++] = 0;
     }
 }
